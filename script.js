09b789e2420472043b59e8ad309131377e21cb73
ndtsman <alanliuyc@gmail.com>
2022-10-02 19:47:48 +1100

    Simplify if statement
    input still needs to line
    
    [Ticket: 1]

--- script.js
+++ script.js
@@ -15,28 +15,24 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    if(playerSelection == "rock" && computerSelection == "rock") {
-        result = "Its a draw!"
-    } else if (playerSelection == "rock" && computerSelection == "paper") {
+    if (playerSelection == "rock" && computerSelection == "paper") {
         result = "You Lose! Paper beats Rock"
     } else if (playerSelection == "rock" && computerSelection == "scissors") {
         result = "You Win! rock beats scissors"
     } else if (playerSelection == "paper" && computerSelection == "rock") {
         result = "You Win! paper beats rock"
-    } else if (playerSelection == "paper" && computerSelection == "paper") {
-        result = "Its a draw!"
     } else if (playerSelection == "paper" && computerSelection == "scissors") {
         result = "You Lose! scissors beats paper"
     } else if (playerSelection == "scissors" && computerSelection == "rock") {
         result = "You Lose! rock beats scissors"
     } else if (playerSelection == "scissors" && computerSelection == "paper") {
         result = "You Win! scissors beats paper"
-    } else {"Its a draw!"}
+    } else { result = "Its a draw!"}
   }
 
 function game() {
     for (let i = 0; i < 5; i++) {
-        playRound(prompt("Choose between rock, paper and scissors"),getComputerChoice())
+        playRound(playerSelection,getComputerChoice())
         console.log(result)
         if (result[4]=="W") {
             playerscore ++
